aa2a43fa215b602f5d90fae07c39b78de78b157a
git hub prectice <git.hub@gmail.com>
2024-03-09 09:40:58 +0530

    Change the structured of User Schema.

--- src/user_information/user.entity.ts
+++ src/user_information/user.entity.ts
@@ -1,17 +1,42 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import validator from 'validator';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop({ type: String, required: [true, 'Name is required'] })
-  name: string;
+  @Prop({
+    type: String,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    required: [true, 'Name is required'],
+  })
+  fullName: string;
 
-  @Prop({ type: String, unique: [true, 'Email already exists'] })
+  @Prop({
+    type: String,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+    unique: [true, 'Email already exists'],
+  })
   email: string;
 
   @Prop({ type: String, required: [true, 'Password is required'] })
   password: string;
+
+  @Prop({ type: String })
+  image: string;
+
+  @Prop({ type: Boolean, default: true })
+  isAdmin: boolean;
+
+  @Prop({ type: String })
+  timestamp: string;
+
+  @Prop({ type: String })
+  token: string;
+
+  @Prop({ type: String, default: new Date().toISOString() })
+  createdAt: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
